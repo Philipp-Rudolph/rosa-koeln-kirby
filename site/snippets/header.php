5019d3fe06b9e39be4b9f2a396873709f27b8395
Philipp-Rudolph <phil.rudolph@ymail.com>
2025-09-12 22:04:23 +0200

    Add favicon images and update header to include multiple icon sizes

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -14,6 +14,11 @@
 
   <!-- Favicon -->
   <link rel="icon" type="image/x-icon" href="/favicon.ico">
+  <!-- Favicon Safari -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+  <!-- <link rel="manifest" href="/site.webmanifest"> -->
 
   <!-- Open Graph -->
   <meta property="og:title" content="<?= $page->title() ?> | <?= $site->title() ?>">
